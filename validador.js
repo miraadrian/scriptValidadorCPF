7894c6398baceccb3d75895f06c3efbe614f38ff
miraadrian <mira.adrian@outlook.com>
2026-09-24 12:56:33 -0300

    Lógica base
    Lógica base para validar CPF.

--- validador.js
+++ validador.js
@@ -20,3 +20,46 @@ x   x  x  x  x  x  x  x  x  x
 se o dígito for maior que 9, se considera 0
 
 */
+let cpf = '705.484.450-52'
+let cpfLimpo = cpf.replace(/\D+/g, '');
+cpfArray = Array.from(cpfLimpo);
+
+
+cpfArray.pop();
+cpfArray.pop();
+// console.log(cpfArray);
+
+//const resultado = cpfArray.map((num, indx) => num * (10 - indx));
+
+// -- Pegando o primeiro Dígito
+const somaTotal1 = cpfArray.reduce((acum, num, indx) => acum + num * (10 - indx), 0);
+console.log(somaTotal1);
+const primeiroDigito = 11 - (somaTotal1 % 11);
+console.log(primeiroDigito);
+
+cpfArray.push(primeiroDigito.toString());
+console.log(cpfArray);
+
+//=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+//pegando o segundo dígito
+const somaTotal2 = cpfArray.reduce((acum, num, indx) => acum + num * (11 - indx), 0);
+console.log(somaTotal2);
+const segundoDigito = 11 - (somaTotal2 % 11);
+console.log(segundoDigito);
+
+cpfArray.push(segundoDigito.toString());
+console.log(cpfArray);
+
+//=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+//Validando o resultado dos get do primeiro e segundo dígito com o CPF inserido incialmente.
+
+const re1 = cpfArray.reduce((acc, item) => acc + item, "");
+console.log(re1);
+
+const validador = function(){
+    return re1 === cpfLimpo ? true : false;
+}
+
+console.log(validador());
